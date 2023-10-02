ab0eab1ef1eb4a5120ad4f972f768ebf4b2e641b
devfemii <144507574+devfemii@users.noreply.github.com>
2023-10-02 04:39:25 -0700

    Update App.js
    Changed path in app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/vid' element={<Video />} />
+        <Route path='/' element={<Video />} />
+        <Route path='/vid' element={<Homepage />} />
       </Routes>
     </div>
   );
